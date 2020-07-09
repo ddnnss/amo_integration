a36cd5b94b2f6571f52649152e6b079e64c6e39c
xxx <sdfds@dsds.tt>
2020-07-09 09:23:30 +0300

    new model

--- amo/views.py
+++ amo/views.py
@@ -22,45 +22,45 @@ def print_log(text):
 @csrf_exempt
 def hook(request):
     print_log('New request from AMO')
-    req = request.POST
-    # req =  {'leads[add][0][id]': ['939197'],
-    #         'leads[add][0][name]': [''],
-    #         'leads[add][0][status_id]': ['33710260'],
-    #         'leads[add][0][price]': ['0'],
-    #         'leads[add][0][responsible_user_id]': ['6130027'],
-    #         'leads[add][0][last_modified]': ['1594206823'],
-    #         'leads[add][0][modified_user_id]': ['6130027'],
-    #         'leads[add][0][created_user_id]': ['6130027'],
-    #         'leads[add][0][date_create]': ['1594206823'],
-    #         'leads[add][0][pipeline_id]': ['3369178'],
-    #         'leads[add][0][account_id]': ['28942735'],
-    #         'leads[add][0][custom_fields][0][id]': ['44991'],
-    #          'leads[add][0][custom_fields][0][name]': ['First Name'],
-    #         'leads[add][0][custom_fields][0][values][0][value]': ['1'],
-    #          'leads[add][0][custom_fields][1][id]': ['45335'],
-    #         'leads[add][0][custom_fields][1][name]': ['Last Name'],
-    #         'leads[add][0][custom_fields][1][values][0][value]': ['2'],
-    #         'leads[add][0][custom_fields][2][id]': ['44995'],
-    #         'leads[add][0][custom_fields][2][name]': ['E-mail'],
-    #         'leads[add][0][custom_fields][2][values][0][value]': ['3'],
-    #         'leads[add][0][custom_fields][3][id]': ['69375'],
-    #         'leads[add][0][custom_fields][3][name]': ['Event'],
-    #         'leads[add][0][custom_fields][3][values][0][value]': ['4'],
-    #         'leads[add][0][custom_fields][4][id]': ['126837'],
-    #         'leads[add][0][custom_fields][4][name]': ['Type of event'],
-    #         'leads[add][0][custom_fields][4][values][0][value]': ['5'],
-    #         'leads[add][0][custom_fields][5][id]': ['69397'],
-    #         'leads[add][0][custom_fields][5][name]': ['Date of the event'],
-    #          'leads[add][0][custom_fields][5][values][0][value]': ['1594206823'],
-    #         'leads[add][0][custom_fields][6][id]': ['127001'],
-    #          'leads[add][0][custom_fields][6][name]': ['Time of the workshop'],
-    #         'leads[add][0][custom_fields][6][values][0][value]': ['1594206823'],
-    #          'leads[add][0][custom_fields][7][id]': ['45285'],
-    #         'leads[add][0][custom_fields][7][name]': ['Ticket number'],
-    #          'leads[add][0][custom_fields][7][values][0][value]': ['8'],
-    #         'leads[add][0][created_at]': ['1594206823'],
-    #          'leads[add][0][updated_at]': ['1594206823'], 'account[subdomain]': ['akamocfcbigideascom'], 'account[id]': ['28942735'],
-    #          'account[_links][self]': ['https://akamocfcbigideascom.amocrm.ru']} dimon.skiborg@gmail.com
+    #req = request.POST
+    req =  {'leads[add][0][id]': ['939197'],
+            'leads[add][0][name]': [''],
+            'leads[add][0][status_id]': ['33710263'],
+            'leads[add][0][price]': ['0'],
+            'leads[add][0][responsible_user_id]': ['6130027'],
+            'leads[add][0][last_modified]': ['1594206823'],
+            'leads[add][0][modified_user_id]': ['6130027'],
+            'leads[add][0][created_user_id]': ['6130027'],
+            'leads[add][0][date_create]': ['1594206823'],
+            'leads[add][0][pipeline_id]': ['3369178'],
+            'leads[add][0][account_id]': ['28942735'],
+            # 'leads[add][0][custom_fields][0][id]': ['44991'],
+             'leads[add][0][custom_fields][0][name]': ['First Name'],
+            'leads[add][0][custom_fields][0][values][0][value]': ['First'],
+             # 'leads[add][0][custom_fields][1][id]': ['45335'],
+            'leads[add][0][custom_fields][1][name]': ['Last Name'],
+            'leads[add][0][custom_fields][1][values][0][value]': ['Last'],
+            'leads[add][0][custom_fields][2][id]': ['44995'],
+            'leads[add][0][custom_fields][2][name]': ['E-mail'],
+            'leads[add][0][custom_fields][2][values][0][value]': ['E-mail'],
+            'leads[add][0][custom_fields][3][id]': ['69375'],
+            'leads[add][0][custom_fields][3][name]': ['Event'],
+            'leads[add][0][custom_fields][3][values][0][value]': ['Event'],
+            'leads[add][0][custom_fields][4][id]': ['126837'],
+            'leads[add][0][custom_fields][4][name]': ['Type of event'],
+            'leads[add][0][custom_fields][4][values][0][value]': ['workshop'],
+            'leads[add][0][custom_fields][5][id]': ['133731'],
+            'leads[add][0][custom_fields][5][name]': ['Date of the event'],
+             'leads[add][0][custom_fields][5][values][0][value]': ['1594206823'],
+            'leads[add][0][custom_fields][6][id]': ['127001'],
+             'leads[add][0][custom_fields][6][name]': ['Time of the workshop'],
+            'leads[add][0][custom_fields][6][values][0][value]': ['1594206823'],
+             'leads[add][0][custom_fields][7][id]': ['45285'],
+            'leads[add][0][custom_fields][7][name]': ['Ticket number'],
+             'leads[add][0][custom_fields][7][values][0][value]': ['Ticket number'],
+            'leads[add][0][created_at]': ['1594206823'],
+             'leads[add][0][updated_at]': ['1594206823'], 'account[subdomain]': ['akamocfcbigideascom'], 'account[id]': ['28942735'],
+             'account[_links][self]': ['https://akamocfcbigideascom.amocrm.ru']}
 
     if req['leads[add][0][status_id]'][0] == '33710263':  #33710263
         print_log('New item in ticket holders')
@@ -74,47 +74,51 @@ def hook(request):
         type_of_event = None
         date_of_event = None
         time_of_the_workshop = None
-        try:
-            first_name = req['leads[add][0][custom_fields][0][values][0][value]'][0]
-        except:
-            print_log('first_name not set')
 
-        try:
-            last_name = req['leads[add][0][custom_fields][1][values][0][value]'][0]
-        except:
-            print_log('last_name not set')
+        for i in range(0,10):
+            id=None
+            try:
+                id = req[f'leads[add][0][custom_fields][{i}][id]'][0]
+            except:
+                pass
 
-        try:
-            email = req['leads[add][0][custom_fields][2][values][0][value]'][0]
-        except:
-            print_log('email not set')
+            if id:
+                if id == '44991':
+                    first_name = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('first_name set')
 
-        try:
-            event = req['leads[add][0][custom_fields][3][values][0][value]'][0]
-        except:
-            print_log('event not set')
+                if id == '45335':
+                    last_name = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('last_name set')
 
-        try:
-            type_of_event = req['leads[add][0][custom_fields][4][values][0][value]'][0]
-        except:
-            print_log('type_of_event not set')
+                if id == '44995':
+                    email = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('email set')
 
-        try:
-            date_of_event = utc_dt = datetime.utcfromtimestamp(int(req['leads[add][0][custom_fields][5][values][0][value]'][0])).replace(tzinfo=pytz.utc)
-            print_log(f'date_of_event {date_of_event}')
-        except:
-            print_log('date_of_event not set')
+                if id == '69375':
+                    event = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('event set')
 
-        try:
-            time_of_the_workshop =  datetime.utcfromtimestamp(int(req['leads[add][0][custom_fields][6][values][0][value]'][0])).replace(tzinfo=pytz.utc)
-            print_log(f'time_of_the_workshop {time_of_the_workshop}')
-        except:
-            print_log('time_of_the_workshop not set')
+                if id == '126837':
+                    type_of_event = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('type_of_event set')
 
-        try:
-            ticket_number = req['leads[add][0][custom_fields][7][values][0][value]'][0]
-        except:
-            print_log('ticket_number not set')
+                if id == '133731':
+                    date_of_event = datetime.utcfromtimestamp(int(req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0])).replace(tzinfo=pytz.utc)
+                    print_log('date_of_event set')
+
+                if id == '127001':
+                    time_of_the_workshop = datetime.utcfromtimestamp(int(req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0])).replace(tzinfo=pytz.utc)
+                    print_log('time_of_the_workshop set')
+
+                if id == '45285':
+                    ticket_number = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('ticket_number set')
+
+                if id == '46517':
+                    tiket_url = req[f'leads[add][0][custom_fields][{i}][values][0][value]'][0]
+                    print_log('tiket_url set')
+
         random_string = ''.join(choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=8))
         url = f'{random_string}--{ticket_number}'
         new_ticket = TicketHolder.objects.create(first_name=first_name,
