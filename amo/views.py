99ad83e54f2591e4594482aa9076e5bbdac160b8
ddnnss <ddnnss.i1@gmail.com>
2020-07-14 12:14:56 +0300

    add email seend

--- amo/views.py
+++ amo/views.py
@@ -62,6 +62,7 @@ def hook(request):
         local_tz = pytz.timezone("Europe/Paris")
         first_name = None
         last_name = None
+        name = None
         amo_number = None
         email = None
         ticket_number = None
@@ -115,40 +116,64 @@ def hook(request):
         contact_id = r.json()['_embedded']['links'][0]['to_entity_id']
         r = requests.get(f"https://akamocfcbigideascom.amocrm.ru/api/v4/contacts/{contact_id}",
                          headers={"Authorization": f'Bearer {access_token}'})
-        first_name = r.json()['first_name']
-        last_name = r.json()['last_name']
 
+        try:
+            name = r.json()['name']
+        except:
+            pass
 
-        new_ticket = TicketHolder.objects.create(first_name=first_name,
-                                    last_name=last_name,
-                                    amo_number=req['leads[add][0][id]'],
-                                    email=email,
-                                    ticket_number=ticket_number,
-                                    ticket_url=url,
-                                    type_of_event=type_of_event,
-                                    event=event,
-                                    date_of_event=date_of_event,
-                                    time_of_the_workshop=time_of_the_workshop)
-        print_log(f'New tickes is : {new_ticket.id}')
-        print_log(f'Sending E-mail to : {email}')
+        try:
+            first_name = r.json()['first_name']
+        except:
+            pass
+        try:
+            last_name = r.json()['last_name']
+        except:
+            pass
 
-        event = Event.objects.first()
+        if not first_name:
+            first_name = name
 
-        ticket_url = f'https://mailsender.inclusionforum.global/ticket/{url}/'
-        msg_html = render_to_string('reg.html', {
-            'event_id':event.event_id,
-            'first_name':first_name,
-            'ticket_number': ticket_number,
-            'ticket_url':  ticket_url,
-        })
+        ticket = None
+        try:
+            tiket = TicketHolder.objects.get(amo_number=req['leads[add][0][id]'])
+        except:
+            pass
 
-        send_mail(f'Registration', None, 'tickets@inclusionforum.global',
-                  [email],
-                  fail_silently=False, html_message=msg_html)
-        send_mail(f'Registration', None, 'tickets@inclusionforum.global',
-                  ['tickets-sent@inclusionforum.global'],
-                  fail_silently=False, html_message=msg_html)
-        print_log(f'Sending E-mail to : tickets-sent@inclusionforum.global')
+        if not ticket:
+            print_log('Creating ticket record')
+            new_ticket = TicketHolder.objects.create(first_name=first_name,
+                                        last_name=last_name,
+                                        amo_number=req['leads[add][0][id]'],
+                                        email=email,
+                                        ticket_number=ticket_number,
+                                        ticket_url=url,
+                                        type_of_event=type_of_event,
+                                        event=event,
+                                        date_of_event=date_of_event,
+                                        time_of_the_workshop=time_of_the_workshop)
+            print_log(f'New tickes is : {new_ticket.id}')
+            print_log(f'Sending E-mail to : {email}')
+
+            event = Event.objects.first()
+
+            ticket_url = f'https://mailsender.inclusionforum.global/ticket/{url}/'
+            msg_html = render_to_string('reg.html', {
+                'event_id':event.event_id,
+                'first_name':first_name,
+                'ticket_number': ticket_number,
+                'ticket_url':  ticket_url,
+            })
+
+            send_mail(f'Registration', None, 'tickets@inclusionforum.global',
+                      [email],
+                      fail_silently=False, html_message=msg_html)
+            send_mail(f'Registration', None, 'tickets@inclusionforum.global',
+                      ['tickets-sent@inclusionforum.global'],
+                      fail_silently=False, html_message=msg_html)
+            print_log(f'Sending E-mail to : tickets-sent@inclusionforum.global')
+        else:
+            print_log('Ticket record exists')
 
     else:
         print_log('This is NOT item in ticket holders')
